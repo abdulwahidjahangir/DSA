b3d27d6dca74092200d6484f2e474d3942547a9c
Abdul Wahid Jahangir <abdulwahidjahangir12@outlook.com>
2026-01-02 19:18:38 +0100

    Reorder List

---
+++ TwoPointersAndSlidingWindow/ReorderList.java
@@ -0,0 +1,72 @@
+package TwoPointersAndSlidingWindow;
+
+public class ReorderList {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        ListNode mid = findMid(head);
+
+        ListNode second = reverseList(mid.next);
+        mid.next = null;
+
+        mergeAlternating(head, second);
+    }
+
+    private void mergeAlternating(ListNode first, ListNode second) {
+        while (first != null && second != null) {
+            ListNode temp1 = first.next;
+            ListNode temp2 = second.next;
+
+            first.next = second;
+            second.next = temp1;
+
+            first = temp1;
+            second = temp2;
+        }
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    private ListNode findMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
