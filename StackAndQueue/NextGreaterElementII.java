503963acc99ea930afb03f2632890cb97c3b3e49
Abdul Wahid Jahangir <abdulwahidjahangir12@outlook.com>
2026-01-07 21:10:13 +0100

    Next Greater Element II

---
+++ StackAndQueue/NextGreaterElementII.java
@@ -0,0 +1,25 @@
+package StackAndQueue;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class NextGreaterElementII {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < 2 * n; i++) {
+            int idx = i % n;
+            while (!st.isEmpty() && nums[st.peek()] < nums[idx]) {
+                ans[st.pop()] = nums[idx];
+            }
+            if (i < n) {
+                st.push(idx);
+            }
+        }
+        return ans;
+    }
+}
