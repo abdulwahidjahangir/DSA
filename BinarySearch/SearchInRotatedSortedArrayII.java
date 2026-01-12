ce9da9596f70275ec72805bccaf729062672630b
Abdul Wahid Jahangir <abdulwahidjahangir12@outlook.com>
2026-01-12 19:08:21 +0100

    Search in Rotated Sorted Array II

---
+++ BinarySearch/SearchInRotatedSortedArrayII.java
@@ -0,0 +1,40 @@
+package BinarySearch;
+
+public class SearchInRotatedSortedArrayII {
+    public boolean search(int[] nums, int target) {
+        int st = 0;
+        int end = nums.length - 1;
+
+        int mid = st + ((end - st) / 2);
+
+        while (st <= end) {
+            if (nums[mid] == target) {
+                return true;
+            }
+
+            if (nums[st] == nums[mid] && nums[mid] == nums[end]) {
+                st++;
+                end--;
+                continue;
+            }
+
+            if (nums[st] <= nums[mid]) {
+                if (nums[st] <= target && target <= nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    st = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[end]) {
+                    st = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+
+            mid = st + ((end - st) / 2);
+        }
+
+        return false;
+    }
+}
