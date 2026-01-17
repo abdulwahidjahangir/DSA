94ce8d252e1091be03fe131647d4dbe1bb7ba8d8
Abdul Wahid Jahangir <abdulwahidjahangir12@outlook.com>
2026-01-17 23:19:03 +0100

    Minimum Size Subarray Sum

---
+++ BinarySearch/MinimumSizeSubarraySum.java
@@ -0,0 +1,22 @@
+package BinarySearch;
+
+public class MinimumSizeSubarraySum {
+    public int minSubArrayLen(int target, int[] nums) {
+        int minLength = Integer.MAX_VALUE;
+
+        int i = 0;
+        int j = 0;
+
+        int currentSum = 0;
+        while (i < nums.length) {
+            currentSum += nums[i++];
+
+            while (currentSum >= target) {
+                minLength = Math.min(i - j, minLength);
+                currentSum -= nums[j++];
+            }
+        }
+
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    }
+}
